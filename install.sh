1b1a711f4ed81cfb57388be9c30c14c320eb45db
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-11-18 15:46:46 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Author      : Dreifus-404
+# Description : This code verify installed packages
+
+actual_dir=$(pwd)
+cd $PREFIX/bin
+
+clear
+
+if [ -x python ] || [ -x python3.10]
+then
+    echo -e "\e[32m [+] Python isn't installed!\e[0m"
+    echo -e "\e[33m [.] Loading program...\e[0m"
+
+    sleep 1.5
+
+    cd $actual_dir
+    python hack.py
+
+else
+    echo -e "\e[32m [-] Python can't be installed, Installing it\e[0m"
+
+    if apt-get install git -y
+    then
+        echo -e "\e[32m [+] Python isn't installed!\e[0m"
+        echo -e "\e[33m [.] Loading program...\e[0m"
+
+        sleep 1.5
+
+        cd $actual_dir
+        python hack.py
+
+    else
+        echo -e " \e[1;31m[-] error check your connection and try again!!\a\e[0m"
+        sleep 1.5
+    fi
+
+fi
