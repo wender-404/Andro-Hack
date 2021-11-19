f369a84cf3545ba4a684a3c2ec624c231dadd1f4
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-11-19 17:26:00 -0300

    Create hack.py

---
+++ __modules__/hack.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/ python
+
+# Author : Dreifus-404
+
+from os import popen
+from time import sleep
+
+popen("whiptail --ascii-lines --title 'WARNING' --msgbox '\n   your cell phone has been hacked' 7 50 --stdout")
+print("\a\a")
