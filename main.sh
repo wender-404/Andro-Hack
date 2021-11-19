b5911d4d3fa1507360abc932569e4ddc95f48709
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-11-19 17:21:42 -0300

    Update main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bash __modules__/get_info.sh; clear
+
+while [ $? -eq 0 ]
+do
+    echo -e "\a\a"
+    ( $( whiptail --ascii-lines --title "©2021 ®AndroHack" --msgbox "\nPress enter for Hack Android!\n" 7 50 --stdout ); $(python __modules__/hack.py) ) &
+done
