dc178ffef9dee6fe10ba375d88d9ec3c663d923c
Wender Santos <77207608+wender-404@users.noreply.github.com>
2022-04-30 16:28:12 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,5 +5,5 @@ bash __modules__/get_info.sh; clear
 while [ $? -eq 0 ]
 do
     echo -e "\a\a"
-    ( $( whiptail --ascii-lines --title "©2021 ®AndroHack" --msgbox "\nPress enter for Hack Android!\n" 7 50 --stdout ); $(python __modules__/hack.py) ) &
+    ( $( dialog --ascii-lines --title "©2021 ®AndroHack" --msgbox "\nPress enter for Hack Android!\n" 7 50 --stdout ); $(python __modules__/hack.py) ) &
 done
